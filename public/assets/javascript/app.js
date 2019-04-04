8c8aed6698e3cb926e90a0e1a2071b4c01325cea
jbramble18 <jbramble.rls@gmail.com>
2019-04-04 14:34:15 -0500

    Scraping-assignment

---
+++ public/assets/javascript/app.js
@@ -0,0 +1,174 @@
+$(document).on("click", "#scrape", function () {
+  $.get("/scrape", function (req, res) {
+      console.log(res);
+    })
+    .then(function (data) {
+      window.location.href = "/";
+    });
+});
+
+$(document).on("click", ".home", function () {
+  $.get("/", function (req, res) {
+    console.log(res);
+  }).then(function (data) {
+    window.location.href = "/";
+  });
+});
+
+$(document).on("click", ".save", function (event) {
+  $(this).parent().remove();
+  var articleId = $(this).attr("data-id");
+  $.ajax({
+      url: "/save/" + articleId,
+      type: "POST"
+    })
+    .done(function (data) {
+      $(".article").filter("[data-id='" + articleId + "']").remove();
+    });
+});
+
+$(document).on("click", ".unsave", function () {
+  $(this).parent().remove();
+  var articleId = $(this).attr("data-id");
+
+  $.ajax({
+      url: "/unsave/" + articleId,
+      type: "POST"
+    })
+    .done(function (data) {
+      $(".article").filter("[data-id='" + articleId + "']").remove();
+    });
+});
+
+$(document).on("click", ".addNote", function (event) {
+  $("#notes").empty();
+  var thisId = $(this).attr("data-id");
+
+  $.ajax({
+    method: "GET",
+    url: "/getNotes/" + thisId
+  }).then(function (data) {
+    console.log(data);
+    $("#notes").append("<h2>" + data.title + "</h2>");
+    $("#notes").append("<h3 id = 'notestitle'></h3>");
+    $("#notes").append("<p id = 'notesbody'></p>");
+    $("#notes").append("<div class='form-group'><label for='title'>Title: </label><input id='titleinput' class='form-control' name='title'></div>");
+    $("#notes").append("<div class='form-group'><label for='body'>Note: </label><input id='bodyinput' class='form-control' name='body'></div>");
+    $("#notes").append("<button class='btn btn-default' data-id='" + data._id + "' id='savenote'>Save Note</button>");
+
+    if (data.note) {
+      $("#notestitle").text(data.note.title);
+      $("#notesbody").text(data.note.body);
+    }
+  });
+  $('#noteModal').modal();
+});
+
+$(document).on("click", "#savenote", function() {
+  var thisId = $(this).attr("data-id");
+
+  $.ajax({
+    method: "POST",
+    url: "/createNote/" + thidId,
+    data: {
+      title: $("#titleinput").val(),
+      body: $("#bodyinput").val()
+    }
+  })
+  .then(function(data) {
+    console.log(data);
+    $("#notes").empty();
+  });
+
+  $("#titleinput").val("");
+  $("#bodyinput").val("");
+  $("#noteModal").modal("hide");
+});
+
+
+
+// // Whenever someone clicks a p tag
+// $(document).on("click", "p", function() {
+//   // Empty the notes from the note section
+//   $("#notes").empty();
+//   // Save the id from the p tag
+//   var thisId = $(this).attr("data-id");
+
+//   // Now make an ajax call for the Article
+//   $.ajax({
+//     method: "GET",
+//     url: "/articles/" + thisId
+//   })
+//     // With that done, add the note information to the page
+//     .then(function(data) {
+//       console.log(data);
+//       // The title of the article
+//       $("#notes").append("<h2>" + data.title + "</h2>");
+//       // An input to enter a new title
+//       $("#notes").append("<input id='titleinput' name='title' >");
+//       // A textarea to add a new note body
+//       $("#notes").append("<textarea id='bodyinput' name='body'></textarea>");
+//       // A button to submit a new note, with the id of the article saved to it
+//       $("#notes").append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
+
+//       // If there's a note in the article
+//       if (data.note) {
+//         // Place the title of the note in the title input
+//         $("#titleinput").val(data.note.title);
+//         // Place the body of the note in the body textarea
+//         $("#bodyinput").val(data.note.body);
+//       }
+//     });
+// });
+
+// // When you click the savenote button
+// $(document).on("click", "#savenote", function() {
+//   // Grab the id associated with the article from the submit button
+//   var thisId = $(this).attr("data-id");
+
+//   // Run a POST request to change the note, using what's entered in the inputs
+//   $.ajax({
+//     method: "POST",
+//     url: "/articles/" + thisId,
+//     data: {
+//       // Value taken from title input
+//       title: $("#titleinput").val(),
+//       // Value taken from note textarea
+//       body: $("#bodyinput").val()
+//     }
+//   })
+//     // With that done
+//     .then(function(data) {
+//       // Log the response
+//       console.log(data);
+//       // Empty the notes section
+//       $("#notes").empty();
+//     });
+
+//   // Also, remove the values entered in the input and textarea for note entry
+//   $("#titleinput").val("");
+//   $("#bodyinput").val("");
+// });
+
+// $(".delete").on("click", function () {
+//   var thisId = $(this).attr("data-id");
+//   $.ajax({
+//     method: "POST",
+//     url: "/articles/delete/" + thisId
+//   }).done(function(data) {
+//     window.location = "/saved"
+//   })
+// });
+
+// $(".deleteNote").on("click", function () {
+//   var noteId = $(this).attr("data-note-id");
+//   var articleId = $(this).attr("data-artcile-id");
+//   $.ajax({
+//     method: "DELETE",
+//     url: "/notes/delete/" + noteId + "/" + articleId
+//   }).done(function(data) {
+//     console.log(data)
+//     $(".modalNote").modal("hide");
+//     window.location = "/saved"
+//   })
+// });
---
+++ server.js
@@ -0,0 +1,175 @@
+var express = require("express");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+var exphbs = require("express-handlebars");
+
+// Our scraping tools
+// Axios is a promised-based http library, similar to jQuery's Ajax method
+// It works on the client and on the server
+var axios = require("axios");
+var cheerio = require("cheerio");
+
+// Require all models
+var db = require("./models");
+
+var PORT = 3000;
+
+// Initialize Express
+var app = express();
+
+// Configure middleware
+
+// Use morgan logger for logging requests
+app.use(logger("dev"));
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Make public a static folder
+app.use(express.static("public"));
+
+
+app.engine("handlebars", exphbs({
+  defaultLayout: "main"
+
+}));
+
+app.set("view engine", "handlebars");
+// Connect to the Mongo DB
+mongoose.connect("mongodb://localhost/newsdb", { useNewUrlParser: true });
+
+// Routes
+
+// A GET route for scraping the echoJS website
+app.get("/", function (req, res) {
+  db.Article
+  .find({saved: false})
+  .then(function(dbArticle) {
+    var hbsObject = {
+      articles: dbArticle
+    };
+    res.render("index", hbsObject);
+  })
+  .catch(function(err) {
+    res.json(err);
+  });
+});
+
+app.get("/all", function(req,res) {
+  db.Article
+  .find({})
+  .then(function(dbArticle){
+    res.json(dbArticle)
+  })
+  .catch(function(err){
+    res.json(err)
+  });
+});
+
+
+// A GET route for scraping the echoJS website
+app.get("/scrape", function(req, res) {
+  // First, we grab the body of the html with axios
+  axios.get("http://www.kansascity.com/latest-news").then(function(response) {
+    // Then, we load that into cheerio and save it to $ for a shorthand selector
+    var $ = cheerio.load(response.data);
+
+    // Now, we grab every h2 within an article tag, and do the following:
+    $(".card").each(function(i, element) {
+      // Save an empty result object
+      var result = {};
+
+      // Add the text and href of every link, and save them as properties of the result object
+      result.title = $(this)
+        .children(".package")
+        .children("h3")
+        .text()
+        .trim();
+      result.link = $(this)
+        .children(".package")
+        .children("h3")
+        .children("a")
+        .attr("href");
+
+      // Create a new Article using the `result` object built from scraping
+      db.Article.create(result)
+        .then(function(dbArticle) {
+          // View the added result in the console
+          console.log(dbArticle);
+        })
+        .catch(function(err) {
+          // If an error occurred, log it
+          console.log(err);
+        });
+    });
+
+    // Send a message to the client
+    res.send("Scrape Complete");
+  });
+});
+
+app.post("/save/:id", function(req,res) {
+  db.Article
+  .findOneAndUpdate({ _id: req.params.id }, { saved: true })
+  .then(function(dbArticle) {
+    res.json(dbArticle);
+  })
+  .catch(function(err) {
+    res.json(err);
+  });
+});
+
+app.post("/unsave/:id", function(req,res) {
+  db.Article
+  .findOneAndUpdate({ _id: req.params.id }, { saved: false })
+  .then(function(dbArticle) {
+    res.json(dbArticle);
+  })
+  .catch(function(err) {
+    res.json(err);
+  });
+});
+
+app.get("/saved", function(req, res) {
+  db.Article
+  .find({ saved: true })
+  .then(function(dbArticle) {
+    var hbsObject = {
+      articles: dbArticle
+    };
+    res.render("saved", hbsObject);
+  })
+  .catch(function(err) {
+    res.json(err);
+  });
+});
+
+app.get("/getNotes/:id", function (req, res) {
+  db.Article
+  .findOne({ _id: req.params.id })
+  .populate("note")
+  .then(function(dbArticle) {
+    res.json(dbArticle);
+  })
+  .catch(function(err) {
+    res.json(err);
+  });
+});
+
+app.post("/createNote/:id", function (req, res) {
+  db.Note
+  .create(req.body)
+  .then(function(dbNote) {
+    return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true});
+  })
+  .then(function(dbArticle) {
+    res.json(dbArticle);
+  })
+  .catch(function(err) {
+    res.json(err);
+  });
+});
+
+// Start the server
+app.listen(PORT, function() {
+  console.log("App running on port " + PORT + "!");
+});
